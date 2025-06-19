433aadd8c0e8ab3a0e74a2c14ca9476778e6ac67
Qvabby <saluqvadze2006@gmail.com>
2025-06-19 16:14:40 +0400

    Way To Expert 2

---
+++ wayToExpert2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wayToExpert2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int page = 36;
+
+
+
+
+
+
+            Console.WriteLine($"last page = {page}");
+        }
+    }
+}
