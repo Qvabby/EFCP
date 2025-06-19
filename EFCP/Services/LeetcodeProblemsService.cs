83cb4bd1e711a06fed353892e04b44d53653e662
Qvabby <saluqvadze2006@gmail.com>
2025-06-19 16:45:50 +0400

    Added Problems and their status, also Leetcode Menu

--- EFCP/Services/LeetcodeProblemsService.cs
+++ EFCP/Services/LeetcodeProblemsService.cs
@@ -1,4 +1,6 @@
-﻿using EFCP.Interfaces;
+﻿using EFCP.HelperMethods;
+using EFCP.Interfaces;
+using qvabbytesD1;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,42 @@ namespace EFCP.Services
 {
     public class LeetcodeProblemsService : ILeetcodeProblemsService
     {
+        ConsoleMessagesWriter CMW = new ConsoleMessagesWriter();
+        ConsoleOutputVisualizer visualizer = new ConsoleOutputVisualizer();
+        MenuHelperMethods menuHelperMethods;
         public async Task LeetCodeProblemsMenuAsync()
         {
-            throw new NotImplementedException();
+			try
+			{
+                Console.Clear();
+                menuHelperMethods = new MenuHelperMethods(visualizer);
+                Console.ResetColor();
+                //Printing Menu
+                visualizer.Qprint("Leetcode Problems.", "Green", "White");
+                //Leetcode Problems Menu
+                var optionsNums = menuHelperMethods.printMenuOptions(new List<string> { "1. Solved", "2. Solving", "3. Profile", "4. Go Back"});
+                //Getting Input
+                visualizer.QprintOnLine("\nSelect an option: ", "White");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                var input = Console.ReadLine();
+                visualizer.BreakLine();
+                Console.ResetColor();
+                //Checking Validation
+                if (!menuHelperMethods.isValidInput(input, optionsNums))
+                {
+                    CMW.WriteErrorMessage("Invalid input. Please try again.");
+                    await LeetCodeProblemsMenuAsync();
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception e)
+			{
+                CMW.WriteErrorMessage($"An error occurred while displaying the game menu. Error Message: {e.Message}\nInner:{e.InnerException.Message} ");
+                await LeetCodeProblemsMenuAsync();
+            }
         }
     }
 }
---
+++ LeetCode Problems/Status.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode_Problems;
+using LeetCode_Problems.Arrays;
+using LeetCode_Problems.Strings;
+namespace LeetCode_Problems
+{
+    public class Status
+    {
+        HardProblems HardProblems = new HardProblems();
+        Dictionary<string, (bool,Delegate)> ProblemsAndStatus = new Dictionary<string, (bool, Delegate)>
+        {
+            { "N-Queens", (false, (Func<int, IList<IList<string>>>)((n) => new HardProblems().SolveNQueens(n))) },
+
+            { "FullJustify", (true, (Func<string[], int, IList<string>>)((words, maxWidth) => new HardString().FullJustify(words,maxWidth))) },
+
+            { "findMedianSortedArrays", (true, (Func<int[],int[],double>)((nums1,nums2) => new HardArray().findMedianSortedArrays(nums1,nums2))) },
+        };
+    }
+}
